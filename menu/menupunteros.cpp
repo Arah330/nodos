e25b9fca9d4ca39c90166e54c0e1454bba0fd425
araceli <aracelihernandezjuan330@gmail.com>
2019-09-19 17:19:44 -0500

    este  programa contiene un   menu con funciones las cuales se basan en doscasos  el primero pide agregar un nuevo nodo cuando este este con un valor nulo dicho de otra manera este vacio el segundo caso va imprimir los valores de nuestra arreglo

---
+++ menu/menupunteros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int opcion;
+int main(){
+	int menu;
+	cout<<"\elige una opcion\n\n";
+	cout<<"1.- Agregar\n";
+	cout<<"2.-imprimir\n";
+	cout<<"3.-salir\n";
+	
+	cin>>opcion;
+}
