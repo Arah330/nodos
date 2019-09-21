a68d3b63c48cb582b2cbd4ad027c0342488710d6
araceli <aracelihernandezjuan330@gmail.com>
2019-09-20 22:57:33 -0500

    este programa nos permite trabajar con listas enlazadas mediante un menu que posee dos opciones ,agregar y imprimir  para identificar elnodo de inicio hacemos uso punteros los cuales seran los que iran recorriendolos nodos para decir si exixte un lugar vacio eingres○7r el nuevo dato al hacer esteel recorecorrido tambien nos permitira mostrarnos los datos conforme vaya cambiando de direccion

---
+++ menu/insertarelementoslista.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct Nodo{
+	int dato;
+	Nodo  *siguiente;
+};
+void insertarLista(Nodo *&,int);
+void mostrarLista(Nodo *);
+void menu();
+
+Nodo *lista=NULL;
+
+int main(){
+	
+	menu();
+	
+	
+	
+	getch();
+	return 0;	
+	
+}
+void menu(){
+	int opcion,dato;
+	do{
+		cout<<"\t.::MENU::.\n";
+	
+	    cout<<"1.- Agregar elementos\n";
+        cout<<"2.-Imprimir elementos \n";
+	    cout<<"3.-Salir\n";
+	    cout<<"\elige una opcion\n\n";
+	    cin>>opcion;
+	    
+	    switch(opcion){
+		    case 1:
+			    cout<<"\tIngresa un numero: ";
+	            cin>>dato;
+	            insertarLista(lista,dato);
+	            cout<<"\n";
+	            system("pause");
+			    break;
+		    case 2:
+			    cout<<"Imprimir valores "<<endl;
+			    mostrarLista(lista);
+			    cout<<"\n";
+			    system("pause");
+			    break ;
+			    
+		}
+		system("cls");
+				
+	
+	
+		
+	}while(opcion!=3);
+}
+
+void insertarLista(Nodo *&lista,int valor){
+	Nodo  *nuevo_nodo =new Nodo();
+	nuevo_nodo->dato=valor;
+	
+	Nodo *aux1=lista;
+	Nodo *aux2;
+	
+	while((aux1 != NULL)&&(aux1->dato<valor)){
+		aux2=aux1;
+		aux1=aux1->siguiente;	
+	}
+	
+	if (lista == aux1){
+		lista=nuevo_nodo;
+	}
+	else{
+		aux2->siguiente=nuevo_nodo;
+		
+	}
+	nuevo_nodo->siguiente=aux1;
+	
+	cout<<"\tDato "<<valor<<"agregado correctamente en lista \n";
+}
+
+void mostrarLista(Nodo  *lista){
+	Nodo *actual=new Nodo();
+	
+	actual=lista;
+	while(actual != NULL ){
+		cout<<actual->dato<<"->";
+		actual=actual->siguiente;
+	}
+}
--- menu/menupunteros.cpp
+++ menu/menupunteros.cpp
@@ -1,12 +1,85 @@
 #include <iostream>
 using namespace std;
-int opcion;
-int main(){
+struct Nodo{
+	int valor;
+	struct Nodo *Ptr;
+};
+
+	void agregar(struct nodo*&iniciolista,int valor);
+	void imprimir(struct nodo*&iniciolista);
+int main(){	
+	struct Nodo *inicio;
+	struct Nodo *aux;
+	int opcion;
 	int menu;
 	cout<<"\elige una opcion\n\n";
 	cout<<"1.- Agregar\n";
-	cout<<"2.-imprimir\n";
-	cout<<"3.-salir\n";
+	cout<<"2.-Imprimir\n";
+	cout<<"3.-Salir\n";
 	
 	cin>>opcion;
+    do{
+  	switch(opcion){
+		case 1:
+			cout<<"Agrgar un nuevo nodo"<<endl;
+			cin>>valor;
+			agregar(inicio,valor);
+		break;
+		case 2:
+			cout<<"Imprimir valores "<<endl;
+			while (nodo*apuntador){
+				cout<<"imprimir"<<endl;
+				cin>>valor;
+				cout<<endl;
+		break ;
+		case 3:
+			cout<<"salir "<<endl;
+			break;
+			default: 
+			cout<<endl;
+			cout<<"error"<<endl;		
+	}
+	
+	}
+	}while (opcion!=3);
+	return 0;
+	
 }
+void agregar(struct nodo*&iniciolista,int valor){
+	struct Nodo *aux;
+	struct Nodo *aux2;
+	aux->valor=valor;
+	aux->Ptr=NULL;
+	
+	aux2=iniciolista;
+
+	if(iniciolista==NULL){
+		iniciolista=aux;
+		
+}else{
+
+	while(aux2==NULL){
+		aux2=aux2->Ptr;
+		auxiliar->valor;
+		
+	};
+	
+
+	
+	
+	
+}
+	}
+		
+void imprimirlista(struct nodo*&iniciolista){
+	struct nodo*auxiliar;
+	iniciolista=auxiliar;
+	while(auxiliar!NULL){
+	cout<<auxiliar->valor;
+	auxiliar=auxiliar->Ptr;
+	
+	}
+	
+}
+	
+
